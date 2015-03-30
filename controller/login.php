b6ff77bcc85e1061435002a8b211b969b49b69f4
Prisha21 <prishagupta1995@gmail.com>
2015-03-30 22:26:01 +0530

    login register changed

--- controller/login.php
+++ controller/login.php
@@ -1,15 +1,31 @@
 <?php
-	# obtain form data
+	$servername="localhost"
 	$username = $_POST['username'];
 	$password = $_POST['password'];
 	$userType = $_POST['usertype'];
 	
-	# authentication variable
+	$conn = mysql_connect($servername, $username, $password,$userType);
+	if (!$conn) {}
+    		#die("Connection failed") #mysql_error()
+
+		
+
 	$auth = False;
 	
-	# mysql queries to check for authenticity in the database table 
-	# return $auth = True if authenticated
-	
+	$query=mysql_query("select user_name from auth WHERE pass_word='$password' && user_type='$userType');
+	$exists=mysql_num_rows($query);
+	if($exists==1)
+	{
+		if($query==$username)
+			{
+				$auth=True;
+			}
+		else
+			{
+				$auth=False;
+			}
+	}
+			
 	if($auth == True) {
 		# create session and redirect to user's home page
 		session_start();
--- controller/register.php
+++ controller/register.php
@@ -1,13 +1,28 @@
 <?php
 	# obtain form data
+	$servername="localhost";
 	$username = $_POST['username'];
 	$password = $_POST['password'];
 	$userType = $_POST['usertype'];
+	$name=$_POST['name'];
+	$email=$_POST['email'];
 	
-	# authentication variable
+	$conn=mysql_connect($servername,$username,$password,$userType,$name,$email);
+	if(!$conn){}
+		#die("connection failed") mysql_error()
+
 	$auth = False;
 	
-	# mysql queries to check for authenticity in the database table 
+	# mysql queries to check for registration in the database table 
+	$sql=mysql_query("INSERT INTO auth(user_name,pass_word,user_type,name,email) 
+		VALUES ('$username','$password','$userType','$name','$email')");
+
+	if($conn->query($sql)==True)
+		{echo "Registered Successfully";
+			$auth=True;}
+	else
+		{echo "Not Registered";
+			$auth=False;}
 	# return $auth = True if authenticated
 	
 	if($auth == True) {
