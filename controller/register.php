705e93c193a9a4478ac40bb8185e8ebb8ba3ce45
Prisha21 <prishagupta1995@gmail.com>
2015-03-31 16:39:37 +0530

    register.php sql part updated

--- controller/register.php
+++ controller/register.php
@@ -7,15 +7,17 @@
 	$name=$_POST['name'];
 	$email=$_POST['email'];
 	$dbname="pap"
-	$conn=mysql_connect($servername,$username,$password,$userType,$name,$email,$dbname);
+	$conn=mysql_connect($servername , "root" , "" , $dbname);
 	if(!$conn){}
 		#die("connection failed") mysql_error()
 
 	$auth = False;
 	
-	# mysql queries to check for registration in the database table 
-	$sql=mysql_query("INSERT INTO auth(user_name,pass_word,user_type,name,email) 
-		VALUES ($username,'$password','$userType','$name','$email')");
+	# mysql queries to check for registration in the database table
+	$query="INSERT INTO auth(username,password,user_type,name,email) 
+		VALUES ($username,'$password','$userType','$name','$email');";
+ 
+	$sql=mysql_query($query);
 
 	if($conn->query($sql)==True)
 		{echo "Registered Successfully";
