ff253227205aeccbf3fe58ba995535c33651800d
Rishab Goyal <rishab954@gmail.com>
2015-10-25 22:51:48 +0530

    Update student.php
    di do write the sql query for it

--- controller/add/student.php
+++ controller/add/student.php
@@ -116,30 +116,37 @@
 		} else
 			$pass &= true;
 	}
+	
+	
+	
 	#tempcity
 	if(empty($_POST["tempcity"])) {
 		$pass = false;
 		$tempcityError = "Temporary City is Required";
 	} else {
-		$tempcity = $_POST['gender'];
+		$tempcity = $_POST['tempcity'];
 		if (!preg_match("/^[A-Z]{3,}*$/", $tempcity)) {
 		# check that it contains legitimate characters
 			$pass = false;
 		} else
 			$pass &= true;
-		
+	}	
 		
 	#tempstate
 	if(empty($_POST["tempstate"])) {
 		$pass = false;
 		$tempstateError = "Temporary State is Required";
 	} else {
-		$tempstate = $_POST['gender'];
+		$tempstate = $_POST['tempstate'];
 		if (!preg_match("/^[a-zA-Z]{3,}*$/", $tempstate)) {
 		# check that it contains legitimate characters
 			$pass = false;
 		} else
 			$pass &= true;
+	}	
+		
+		
+		
 	#temppin
 	if(empty($_POST["tpin"])) {
 		$pass = false;
@@ -151,13 +158,149 @@
 			$pass = false;
 		} else
 			$pass &= true;
+	}
+	    
+		
 	#permadd
+	if(empty($_POST["permadd"])) {
+		$pass = false;
+		$permaddError = " Address is Required";
+	} else {
+		$permadd = $_POST['permadd'];
+		if (!preg_match("/^[a-zA-Z][ a-zA-z,.-_/:]*$/", $permadd))  {
+		# check that it contains legitimate characters
+			$pass = false;
+		} else
+			$pass &= true;
+	}
+	
+	
+	
 	#permcity
+	if(empty($_POST["permcity"])) {
+		$pass = false;
+		$permcityError = "Permanent City is Required";
+	} else {
+		$permcity = $_POST['permcity'];
+		if (!preg_match("/^[A-Z]{3,}*$/", $permcity) {
+		# check that it contains legitimate characters
+			$pass = false;
+		} else
+			$pass &= true;
+	}
+	
+	
+	
 	#permstate
+	if(empty($_POST["permstate"])) {
+		$pass = false;
+		$permstateError = "state is Required";
+	} else {
+		$permstate = $_POST['permstate'];
+		if (!preg_match("/^[a-zA-Z]{3,}*$/", $permstate)) {
+		# check that it contains legitimate characters
+			$pass = false;
+		} else
+			$pass &= true;
+	}	
+	
 	#permpin
+	if(empty($_POST["ppin"])) {
+		$pass = false;
+		$permpinError = "Pin is Required";
+	} else {
+		$permpin = $_POST['tpin'];
+		if (!preg_match("/^[0-9{6}*$/", $permpin)) {
+		# check that it contains legitimate characters
+			$pass = false;
+		} else
+			$pass &= true;
+	}
+	   
+	
+	
 	#dadname
+	if(empty($_POST["dadname"])) {
+		$pass = false;
+		$dadnameError = " Name is Required";
+	} else {
+		$dadname = $_POST['dadname'];
+		if (!preg_match("/^[A-Z]{3,}*$/", $dadname) {
+		# check that it contains legitimate characters
+			$pass = false;
+		} else
+			$pass &= true;
+	}
 	#dadoccup
+	if(empty($_POST["permcity"])) {
+		$pass = false;
+		$dadoccup = "Occupoation is Required";
+	} else {
+		$dadoccup= $_POST['dadoccup'];
+		if (!preg_match("/^[A-Z]{3,}*$/", $dadoccup) {
+		# check that it contains legitimate characters
+			$pass = false;
+		} else
+			$pass &= true;
+	}
+	
+	
 	#momname
+	if(empty($_POST["permcity"])) {
+		$pass = false;
+		$momnameError = "Name is Required";
+	} else {
+		$momname = $_POST['momname'];
+		if (!preg_match("/^[A-Z]{3,}*$/", $momname) {
+		# check that it contains legitimate characters
+			$pass = false;
+		} else
+			$pass &= true;
+	}
 	#momoccup
+	if(empty($_POST["momoccup"])) {
+		$pass = false;
+		$momoccupError = "Occupation is Required";
+	} else {
+		$momoccup = $_POST['momoccup'];
+		if (!preg_match("/^[A-Z]{3,}*$/", $momoccup) {
+		# check that it contains legitimate characters
+			$pass = false;
+		} else
+			$pass &= true;
+	}
+	
+	
+	#initalize mysql queries 
+	if($pass){
+		#mysql connection
+		$servername = "localhost";
+		$dbname = "pap";
+		$conn = mysql_connect($servername, "root", "", $dbname);
+		if(!conn){
+			die("connection failed" . mysql_error());
+			
+		}
+		
+		#please write the sql query here
+		$query = "INSERT INTO student()VALUES();";
+		
+		
+		
+		if(mysqli_query($conn, $query) == true) {
+			# new item added successfully
+			echo json_encode(array("auth" => "true"));
+		} else {
+			# registration unsuccessful
+		}
+		
+	} else {
+		echo json_encode(
+			array(
+
+			)
+		);
+		
+	}
 
 ?>
