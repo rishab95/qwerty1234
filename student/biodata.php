293b141a1aabdb4f4c22904cc5bb2665b629143a
Rishab Goyal <rishab954@gmail.com>
2015-11-22 12:33:44 +0530

    Update biodata.php
    UI/UX Updates for the form

--- student/biodata.php
+++ student/biodata.php
@@ -17,8 +17,6 @@
 			}
 		}
 	}
-
-
 ?>
 
 <!IDOCTYPE html>
@@ -36,49 +34,82 @@
 <html>
 	<head>
 		<title>Add biodata</title>
+		
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="../bootstrap/css/bootstrap-theme.min.css">
+        
+        <!-- Latest complied and minified JQuery -->
+        <script src="../bootstrap/jquery-2.1.3.min.js"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="../bootstrap/js/bootstrap.min.js"></script>
+    
+	    <!-- Custom made CSS file -->
+    	<link rel="stylesheet" href="../style.css">
+        
+        <!-- Custom common JQuery file -->
+        <script src="../ess.js"></script>
 	</head>
 <body>
-	<h2>student biodata from</h2>
-		<form method = "post" action = "biodata.php">
-			<div>
-				<label for = "name">Username</label>
-				<input type = "text" name = "Username" value= "">
-				<label for = "name">Year Of Pass</label>
-				<input type = "text" name = "Year Of Pass" value= "">
-				<label for = "name">Branch</label>
-				<input type = "text" name = "Branch" value= "">
-				<label for = "name">DOB</label>
-				<input type = "text" name = "DOB" value= "">
-				<label for = "name">Citizenship</label>
-				<input type = "text" name = "Citizenship" value= "">
-				<label for = "name">Gender</label>
-				<input type = "text" name = "Gender" value= "">
-				<label for = "name">Temporary Address</label>
-				<input type = "text" name = "Temporary Address" value= "">
-				<label for = "name">Temporary City</label>
-				<input type = "text" name = "Temporary City" value= "">
-				<label for = "name">Temporary State</label>
-				<input type = "text" name = "Temporary State" value= "">
-				<label for = "name">Temporary Pin</label>
-				<input type = "text" name = "Temporary Pin" value= "">
-				<label for = "name">Permanent Address</label>
-				<input type = "text" name = "Permanent Address" value= "">
-				<label for = "name">Permanent City</label>
-				<input type = "text" name = "Permanent City" value= "">
-				<label for = "name">Permanent State</label>
-				<input type = "text" name = "Permanent State" value= "">
-				<label for = "name">Permanent Pin</label>
-				<input type = "text" name = "Permanent Pin" value= "">
-				<label for = "name">Father's Name</label>
-				<input type = "text" name = "Father's Name" value= "">
-				<label for = "name">Father's Occupation</label>
-				<input type = "text" name = "Father's Occupation" value= "">
-			    <label for = "name">Mother's Name</label>
-				<input type = "text" name = "Mother's Name" value= "">
-				<label for = "name">Mother's Occupation</label>
-				<input type = "text" name = "Mother's Occupation" value= "">
-				
+	<div class="body2"></div>
+        <div class = "container">	
+			<form method = "post" action = "biodata.php">
+			<div class = "panel panel-default">
+				<div class="panel-heading">
+					<div class="row">
+						<div class="col-md-12">
+							<h3 id="name">Student Biodata Form</h3>
+						</div>
+					</div>
+				</div>
+		
+			
+				<div class = "form group">
+				<div class = "panel-heading">
+									<label for = "name"><h5>Username</h5></label>
+									<input type = "text" name = "Username" value= ""><br>
+									<label for = "name"><h5>Year Of Pass</h5></label>
+									<input type = "text" name = "Year Of Pass" value= ""><br>
+									<label for = "name"><h5>Branch</h5></label>
+									<input type = "text" name = "Branch" value= ""><br>
+									<label for = "name"><h5>DOB</h5></label>
+									<input type = "text" name = "DOB" value= ""><br>
+									<label for = "name"><h5>Citizenship</h5></label>
+									<input type = "text" name = "Citizenship" value= ""><br>
+									<label for = "name"><h5>Gender</h5></label>
+									<input type = "text" name = "Gender" value= ""><br>
+									<label for = "name"><h5>Temporary Address</h5></label>
+									<input type = "text" name = "Temporary Address" value= ""><br>
+									<label for = "name"><h5>Temporary City</h5></label>
+									<input type = "text" name = "Temporary City" value= ""><br>
+									<label for = "name"><h5>Temporary State</h5></label>
+									<input type = "text" name = "Temporary State" value= ""><br>
+									<label for = "name"><h5>Temporary Pin</h5></label>
+									<input type = "text" name = "Temporary Pin" value= ""><br>
+									<label for = "name"><h5>Permanent Address</h5></label>
+									<input type = "text" name = "Permanent Address" value= ""><br>
+									<label for = "name"><h5>Permanent City</h5></label>
+									<input type = "text" name = "Permanent City" value= ""><br>
+									<label for = "name"><h5>Permanent State</h5></label>
+									<input type = "text" name = "Permanent State" value= ""><br>
+									<label for = "name"><h5>Permanent Pin</h5></label>
+									<input type = "text" name = "Permanent Pin" value= ""><br>
+									<label for = "name"><h5>Father's Name</h5></label>
+									<input type = "text" name = "Father's Name" value= ""><br>
+									<label for = "name"><h5>Father's Occupation</h5></label>
+									<input type = "text" name = "Father's Occupation" value= ""><br>
+									<label for = "name"><h5>Mother's Name</h5></label>
+									<input type = "text" name = "Mother's Name" value= ""><br>
+									<label for = "name"><h5>Mother's Occupation</h5></label>
+									<input type = "text" name = "Mother's Occupation" value= ""><br>
+									<button type="button" class="btn btn-primary">Submit</button>
+					</div>	
+				</div>
 			</div>
+		</div>
 		</form>
 </body>
 </html>
